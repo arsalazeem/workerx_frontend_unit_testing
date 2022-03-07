246234a18072de428dbc3705567dc53a9462095e
arsalazeem <arsal.azeem@vizteck.com>
2022-03-07 17:27:23 +0500

    started writing unit tests with selenium python using pyunit

--- login.py
+++ login.py
@@ -4,40 +4,54 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 import unittest
 
-loginscreen={
-    "email":'//input[@id="email"]',
-    "password":'//input[@id="password"]',
-    "login_button":"//button[contains(text(), 'Log In')]"
+loginscreen = {
+    "email": '//input[@id="email"]',
+    "password": '//input[@id="password"]',
+    "login_button": "//button[contains(text(), 'Log In')]"
 }
+
 
 def start():
     try:
+        chrome_options = Options()
+        chrome_options.add_argument('--disable-extensions')
+        chrome_options.add_argument('--headless')
         service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service)
+        driver = webdriver.Chrome(service=service, chrome_options=chrome_options)
         driver.maximize_window()
         return driver
     except Exception as e:
         print(e)
         quit()
 
+
 class TestStringMethods(unittest.TestCase):
+    def setUp(self):
+        self.driver = start()
+        self.driver.implicitly_wait(30)
 
     def test_login(self):
-        driver = start()
-        driver.implicitly_wait(30)
-        driver.get("https://stagingadmin.workerx.co/")
-        driver.find_element(By.XPATH,loginscreen["email"]).send_keys("arsal.azeem@vizteck.com")
-        driver.find_element(By.XPATH, loginscreen["password"]).send_keys("12345678")
-        driver.find_element(By.XPATH, loginscreen["login_button"]).click()
-        print(driver.title)
-        time.sleep(2)
-        self.assertTrue('WorkerX Business',driver.title)
-        driver.quit()
+        self.driver.get("https://stagingadmin.workerx.co/")
+        self.driver.find_element(By.XPATH, loginscreen["email"]).send_keys("arsal.azeem@vizteck.com")
+        self.driver.find_element(By.XPATH, loginscreen["password"]).send_keys("12345678")
+        self.driver.find_element(By.XPATH, loginscreen["login_button"]).click()
+        print(self.driver.title)
+        self.assertTrue('WorkerX Business', self.driver.title)
 
+    def test_login_with_incorrect_password(self):
+        self.driver.get("https://stagingadmin.workerx.co/")
+        self.driver.find_element(By.XPATH, loginscreen["email"]).send_keys("arsal.azeem@vizteck.com")
+        self.driver.find_element(By.XPATH, loginscreen["password"]).send_keys("1234567810")
+        self.driver.find_element(By.XPATH, loginscreen["login_button"]).click()
+        print(self.driver.title)
+        self.assertTrue('WorkerX Business', self.driver.title)
+
+    def tearDown(self):
+        self.driver.quit()
 
 
 if __name__ == '__main__':
     unittest.main()
-
